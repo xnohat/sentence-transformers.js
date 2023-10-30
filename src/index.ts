b8051066deafb6f09fe7d6ec7abba72b6e03d132
Hong Phuc Nguyen <xnohat@gmail.com>
2023-10-31 01:24:02 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import { env } from "@xenova/transformers";
-
-import { SentenceTransformer } from "./model.js";
-
-async function main() {
-  console.log(env);
-  const sentenceTransformer = await SentenceTransformer.from_pretrained(
-    "Supabase/bge-small-en"
-  );
-  await sentenceTransformer.encode(["Hello world", "How are you guys doing?"]);
-}
-
-main();
+export * from './model.ts';
